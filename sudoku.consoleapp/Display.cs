dd99a57f9ffe29f2f1e95016002fc68f88f53b9e
Ismail Osman <78069963+CoolWirya@users.noreply.github.com>
2024-08-04 23:49:54 +0300

    Added colors and two populate methods

---
+++ sudoku.consoleapp/Display.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace sudoku.consoleapp;
+
+public class Display
+{
+
+    private static Dictionary<int,ConsoleColor?> _colors = [];   
+    public static void WriteYellow(string str)
+    {
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.Write(str);
+        Console.ResetColor();
+    }
+    public static void WriteRed(string str)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write(str);
+        Console.ResetColor();
+    }
+    public static void WriteGreen(string str)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.Write(str);
+        Console.ResetColor();
+    }
+    public static void WriteRandomColor(string str)
+    {
+        _ = int.TryParse(str, out int sqrNo);
+        var color = GetColor(sqrNo);
+        if (color is null)
+        {
+            color = GenerateRandomColor();
+            _colors.Add(sqrNo, color);
+        }
+        Console.ForegroundColor = color ?? ConsoleColor.Yellow;
+        Console.Write(str);
+        Console.ResetColor();
+    }
+    public static ConsoleColor GenerateRandomColor()
+    {
+        Random ran = new ();
+        return (ConsoleColor)ran.Next(1, 13);
+    }
+    public static ConsoleColor? GetColor(int sqrNo)
+    {
+        _ = _colors.TryGetValue(sqrNo, out var color);
+        return color;
+    }
+}
+
--- sudoku.consoleapp/SudokuGrid.cs
+++ sudoku.consoleapp/SudokuGrid.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Net.NetworkInformation;
+
 namespace sudoku.consoleapp;
 
 public record SudokuGrid
@@ -8,7 +10,6 @@ public record SudokuGrid
     public Axis Position { get; set; }
 
     private static  Dictionary<SudokuGrid, int> _sudoko = [];
-
     public static Dictionary<SudokuGrid, int> Generate(int[] input)
     {
         for (int x = 0; x < input.Length; x++)
@@ -17,8 +18,7 @@ public record SudokuGrid
             {
                 _sudoko.Add(new()
                 {
-                    SquareNo = (int)(x / Math.Sqrt(input.Max())) * (int) Math.Sqrt(input.Max())
-                                + (int)(y / Math.Sqrt(input.Max())),
+                    SquareNo = CalculateSquareNumber(x,y,input),
                     Position = new () { Y = y, X = x }
                 }, 0
                );
@@ -26,33 +26,148 @@ public record SudokuGrid
         }
         return _sudoko;
     }
-    public static void ShowValues()
+    public static void ShowValues(int[] input)
     {
-        string output = string.Empty;
+        int sqr = input.Length / (int)Math.Sqrt(input.Length);
         Console.WriteLine("Values:");
         foreach (var sudoko in _sudoko)
         {
             if (sudoko.Key.Position.Y == 0)
             {
-                output += "\n";
+                Console.WriteLine();
             }
-            output += $"|({sudoko.Key.Position.X:#00},{sudoko.Key.Position.Y:#00}) {sudoko.Value:#00}";
+            DisplayGridItem(sudoko.Key.Position.X, sudoko.Key.Position.Y, sudoko.Value, sqr);
         }
-        Console.WriteLine(output);
     }
-    public static void ShowDesign()
+    public static void ShowDesign(int[] input)
     {
-        string output = string.Empty;
+        int sqr = input.Length / (int) Math.Sqrt(input.Length);
         Console.WriteLine("Design:");
         foreach (var sudoko in _sudoko)
         {
             if (sudoko.Key.Position.Y == 0)
             {
-                output += "\n";
+                Console.WriteLine();
             }
-            output += $"|({sudoko.Key.Position.X:#00},{sudoko.Key.Position.Y:#00}) {sudoko.Key.SquareNo:#00}";
+           DisplayGridItem(sudoko.Key.Position.X, sudoko.Key.Position.Y, sudoko.Key.SquareNo, sqr, true);
         }
-        Console.WriteLine(output);
+    }
+    public static void PopulateFirstSqrRootRows(int[] input)
+    {
+        for (int x = 0; x < input.Length; x++)
+        {
+            if (x == Math.Sqrt(input.Length))
+            {
+                break;
+            }
+            for (int y = 0; y < input.Length; y++)
+            {
+                var key = new SudokuGrid()
+                {
+                    SquareNo = CalculateSquareNumber(x, y, input),
+                    Position = new Axis() { X = x, Y = y }
+                };
+                if(x == 0)
+                {
+                    _sudoko[key] = input[y];
+                }
+                else
+                {
+                    CalculateAboveItemPlusSqrRootValue(x, y, key, input);
+                }
+                
+            }
+        }
+    }
+    /*
+    (3,0) = (0,2) => x - 3, y + 2
+    (3,1) = (0,0) => x - 3, y - 1
+     */
+    public static void PopulateRemainingRows(int[] input)
+    {
+        foreach (var sqrNumber in _sudoko.Keys.Select(n=>n.SquareNo ).ToList())
+        {
+            if(sqrNumber < Math.Sqrt(input.Length))
+            {
+                continue;
+            }
+            var sqrItems = _sudoko.Keys.Where(n => n.SquareNo == sqrNumber).ToArray();
+            int i = 0;
+            foreach (var sqrItem in sqrItems)
+            {
+                if(i == Math.Sqrt(input.Length))
+                {
+                    i = 0;
+                }
+                if (i == 0)
+                {
+                    _sudoko.TryGetValue(new()
+                    {
+                        SquareNo = sqrNumber - (int) Math.Sqrt(input.Length),
+                        Position = new Axis()
+                        {
+                            X = sqrItem.Position.X - (int)Math.Sqrt(input.Length),
+                            Y = sqrItem.Position.Y + (int)Math.Sqrt(input.Length) - 1
+                        }
+                    }, out var item);
+                    _sudoko[sqrItem] = item;
+                    i++;
+                }
+                else
+                {
+                    _sudoko.TryGetValue(new()
+                    {
+                        SquareNo = sqrNumber - (int)Math.Sqrt(input.Length),
+                        Position = new Axis()
+                        {
+                            X = sqrItem.Position.X - (int)Math.Sqrt(input.Length),
+                            Y = sqrItem.Position.Y  - 1
+                        }
+                    }, out var item);
+                    _sudoko[sqrItem] = item;
+                    i++;
+                }       
+            }
+        }
+    }
+    private static void CalculateAboveItemPlusSqrRootValue(int x, int y, SudokuGrid key, int[] input)
+    {
+        SudokuGrid aboveItemKey = new()
+        {
+            SquareNo = key.SquareNo,
+            Position = new Axis()
+            {
+                X = key.Position.X - 1,
+                Y = key.Position.Y
+            }
+        };
+        _sudoko.TryGetValue(aboveItemKey, out var value);
+        _sudoko[key] = (value + (int) Math.Sqrt(input.Length) > input.Length) 
+            ? value + (int) Math.Sqrt(input.Length) - input.Length 
+            : value + (int)Math.Sqrt(input.Length);
+    }
+    private static int CalculateSquareNumber(int x, int y, int[] input)
+        => (int)(x / Math.Sqrt(input.Max())) * (int)Math.Sqrt(input.Max())+ (int)(y / Math.Sqrt(input.Max()));
+    private static void DisplayGridItem(int x, int y, int value, int sqr, bool isDesign = false)
+    {
+        Console.Write("|(");
+        Display.WriteYellow(x.ToString((sqr <= 3) ? "#0" : "#00"));
+        Console.Write(",");
+        Display.WriteYellow(y.ToString((sqr <= 3) ? "#0" : "#00"));
+        Console.Write(") ");
+        if (value == 0 && !isDesign)
+        {
+            Display.WriteRed(value.ToString((sqr <= 3) ? "#0" : "#00"));
+        }
+        else if (isDesign)
+        {
+            Display.WriteRandomColor(value.ToString((sqr <= 3) ? "#0" : "#00"));
+        }
+        else
+        {
+            Display.WriteGreen(value.ToString((sqr <= 3) ? "#0" : "#00"));
+        }
+
     }
 
 
@@ -63,5 +178,6 @@ public record SudokuGrid
 
 
 
-    
+
+
 }
