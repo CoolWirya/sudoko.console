8a3f4a57c86f70a49b97f5aa9967fc102f5c7a70
Ismail Osman <78069963+CoolWirya@users.noreply.github.com>
2024-08-04 14:29:55 +0300

    Project added

---
+++ sudoku.consoleapp/Axis.cs
@@ -0,0 +1,9 @@
+﻿
+
+namespace sudoku.consoleapp;
+
+public record Axis
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
---
+++ sudoku.consoleapp/SudokoGrid.cs
@@ -0,0 +1,67 @@
+﻿
+
+namespace sudoku.consoleapp;
+
+public record SudokoGrid
+{
+    public int SquareNo { get; set; }
+    public Axis Position { get; set; }
+
+    private static  Dictionary<SudokoGrid, int> _sudoko = [];
+
+    public static Dictionary<SudokoGrid, int> Generate(int[] input)
+    {
+        for (int x = 0; x < input.Length; x++)
+        {
+            for (int y = 0; y < input.Length; y++)
+            {
+                _sudoko.Add(new()
+                {
+                    SquareNo = (int)(x / Math.Sqrt(input.Max())) * (int) Math.Sqrt(input.Max())
+                                + (int)(y / Math.Sqrt(input.Max())),
+                    Position = new () { Y = y, X = x }
+                }, 0
+               );
+            }
+        }
+        return _sudoko;
+    }
+    public static void ShowValue()
+    {
+        string output = string.Empty;
+        Console.WriteLine("Values:");
+        foreach (var sudoko in _sudoko)
+        {
+            if (sudoko.Key.Position.Y == 0)
+            {
+                output += "\n";
+            }
+            output += $"|({sudoko.Key.Position.X:#00},{sudoko.Key.Position.Y:#00}) {sudoko.Value:#00}";
+        }
+        Console.WriteLine(output);
+    }
+    public static void ShowDesign()
+    {
+        string output = string.Empty;
+        Console.WriteLine("Design:");
+        foreach (var sudoko in _sudoko)
+        {
+            if (sudoko.Key.Position.Y == 0)
+            {
+                output += "\n";
+            }
+            output += $"|({sudoko.Key.Position.X:#00},{sudoko.Key.Position.Y:#00}) {sudoko.Key.SquareNo:#00}";
+        }
+        Console.WriteLine(output);
+    }
+
+
+
+
+
+
+
+
+
+    
+}
